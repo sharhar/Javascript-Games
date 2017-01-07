70124578ab94e970eae982ecd0b1a3a83f91ef04
sharhar <wiishahar@gmail.com>
2017-01-06 19:29:16 -0800

    WebGL

--- base.js
+++ base.js
@@ -1,16 +1,71 @@
 var canvas;
 var ctx;
+var gl;
+var fragmentShader;
+var vertexShader;
+var shaderProgram;
+
+function getShader(gl, id) {
+    var shaderScript = document.getElementById(id);
+    if (!shaderScript) {
+      return null;
+    }
+    var str = "";
+    var k = shaderScript.firstChild;
+    while (k) {
+      if (k.nodeType == 3) {
+        str += k.textContent;
+      }
+      k = k.nextSibling;
+    }
+    var shader;
+    if (shaderScript.type == "x-shader/x-fragment") {
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+      shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+      return null;
+    }
+    gl.shaderSource(shader, str);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert(gl.getShaderInfoLog(shader));
+      return null;
+    }
+    return shader;
+  }
 
 function initGame() {
 	canvas = document.getElementById("game-canvas");
 
-	if (canvas.getContext) {
-        ctx = canvas.getContext("2d");
-    } else {
-    	alert("Browser does not support canvas tag");
-    	return;
-    }
+	try {
+		gl = canvas.getContext("experimental-webgl");
+		gl.viewportWidth = canvas.width;
+      	gl.viewportHeight = canvas.height;
 
-    window.requestAnimationFrame(draw);
+      	fragmentShader = getShader(gl, "shader-fs");
+		vertexShader = getShader(gl, "shader-vs");
+		
+		shaderProgram = gl.createProgram();
+    	
+    	gl.attachShader(shaderProgram, vertexShader);
+    	gl.attachShader(shaderProgram, fragmentShader);
+    	gl.linkProgram(shaderProgram);
+
+    	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      		alert("Could not initialise shaders");
+    	}
+
+    	
+	} catch(e) {
+
+	}
+	
+	if(!gl) {
+ 		ctx = canvas.getContext("2d");
+ 		window.requestAnimationFrame(drawCanvas);
+	} else {
+		window.requestAnimationFrame(drawGL);
+	}
 }
 
--- game.js
+++ game.js
@@ -2,12 +2,20 @@ var x = -50;
 var y = -50;
 var time = 0;
 
-function draw() {
-	ctx.save();
+function drawGL() {
+    gl.clearColor(1, 0, 1, 1);
 
-	ctx.setTransform(1, 0, 0, 1, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+    window.requestAnimationFrame(drawGL);
+}
+
+function drawCanvas() {
+    ctx.save();
+
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle="#FF0000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -21,8 +29,8 @@ function draw() {
     ctx.rotate(100*time*Math.PI/180);
     
 
-	ctx.fillRect(x,y,100,100);
-	time += 0.01;
+    ctx.fillRect(x,y,100,100);
+    time += 0.01;
 
-	window.requestAnimationFrame(draw);
+    window.requestAnimationFrame(drawCanvas);
 }
